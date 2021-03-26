a7a825844a9c5532a2c4633db8712476f0e63a74
r.k <rikakato215c0169@gmail.com>
2021-03-26 07:45:46 +0000

    add culum

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  
+before_action :set_task, only:[:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end 
   
   def show
-    @task = Task.find(params[:id])
   end 
   
   def new
@@ -19,16 +21,14 @@ class TasksController < ApplicationController
       redirect_to @task
     else
       flash.now[:danger] = 'Taskが追加されませんでした'
-      render new
+      render :new
     end 
   end 
   
   def edit 
-    @task = Task.find(params[:id])
   end 
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
   end 
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました'
@@ -49,8 +48,12 @@ class TasksController < ApplicationController
   
   private
   
+  def set_task
+    @task = Task.find(params[:id])
+  end 
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end 
   
   
